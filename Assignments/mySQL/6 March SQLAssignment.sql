47e0bfa4a457a3086d9814dbcf042e587e8dd31f
Dharan Tej <pdharantej@teksystems.com>
2023-03-08 12:32:57 +0530

    8th march 2023 SQL Day 5

--- Assignments/mySQL/6 March SQLAssignment.sql
+++ Assignments/mySQL/6 March SQLAssignment.sql
@@ -2,26 +2,90 @@
 -- EmployeeID, Department, JobRole, Attrition, Gender, Age, MaritalStatus, Education, EducationField, BusinessTravel,
 -- JobInvolvement, JobLevel, JobSatisfaction, Hourlyrate, Income, Salaryhike, OverTime, Workex, YearsSinceLastPromotion,
 -- EmpSatisfaction, TrainingTimesLastYear, WorkLifeBalance, Performance_Rating, and set EmployeeID as the primary key.
+create database if not exists hremployeeDB;
+use hremployeeDB;
+-- table imported from import table wizard
 
 -- 2.	Return the shape of the table
--- 3.	Show the count of Employee & percentage Workforce in each Department.
--- 4.	Which gender have higher strength as workforce in each department?
--- 5.	Show the workforce in each Job Role
--- 6.	Show Distribution of Employee's Age Group.
--- 7.	Compare all marital status of employee and find the most frequent marital status.
--- 8.	What is Job satisfaction level of employee?
--- 9.	How frequently employee is going on Business Trip.
--- 10.	Show the Department with Highest Attrition Rate (Percentage)
--- 11.	Show the Job Role with Highest Attrition Rate (Percentage)
--- 12.	Show Distribution of Employee's Promotion, Find the maximum chances of employee getting promoted.
--- 13.	Find the Attrition Rate for Marital Status.
--- 14.	Find the Attrition Count & Percentage for Different Education Levels
--- 15.	Find the Attrition & Percentage Attrition for Business Travel.
--- 16.	Find the Attrition & Percentage Attrition for Various JobInvolvement
--- 17.	Show Attrition Rate for Different JobSatisfaction.
--- 18.	Find key reasons for Attrition in Company.
--- 19.	Return all employee where WorkEx greater than 10, provided that they travel frequently, WorkLifeBalance 
--- as Good and JobSatisfaction is Very High.
+select count(*) as size_of_table from hr_employee;
 
--- 20.	 Write query to display who has better WorkLifeBalance , Married, Single or Divorced provided that 
--- Performace_Rating is Outstanding. 
+-- 3.	Show the count of Employee & percentage Workforce in each Department.
+select department,count(*) as dept_count from hr_employee group by department order by department;
+
+-- 4.	Which gender have higher strength as workforce in each department?
+select gender,department,count(*) as gender_count from hr_employee group by gender,department order by gender;
+
+-- 5.	Show the workforce in each Job Role
+select jobrole,count(*) as workforce from hr_employee group by jobrole order by jobrole;
+
+-- 6. Show Distribution of Employee's Age Group.
+select age,count(*) as age_group from hr_employee group by age order by age;
+
+-- 7.	Compare all marital status of employee and find the most frequent marital status.
+select maritalstatus,count(*) as most_fregquent  from hr_employee group by maritalstatus;
+
+-- 8. What is Job satisfaction level of employee?
+select jobsatisfaction,count(*) as job_satisfaction_rate from hr_employee group by jobsatisfaction;
+
+-- 9.How frequently employee is going on Business Trip.
+select businesstravel,count(*) as business_travel_rate from hr_employee group by businesstravel;
+
+-- 10.	Show the Department with Highest Attrition Rate (Percentage)
+select department, COUNT(*) AS total_count, SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count,(SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS attrition_rate from hr_employee group by department order by attrition_rate DESC LIMIT 5;
+
+-- 11 .Show the Job Role with Highest Attrition Rate (Percentage)
+select jobrole, COUNT(*) AS total_count, SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count,(SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS attrition_rate
+from hr_employee
+group by jobrole
+order by attrition_rate DESC
+LIMIT 5;
+
+-- 12.Show Distribution of Employee's Promotion, Find the maximum chances of employee getting promoted
+select yearssincelastpromotion,count(*) as promotion_distribution 
+from hr_employee 
+group by yearssincelastpromotion order by yearssincelastpromotion asc;
+
+-- 13 Find the Attrition Rate for Marital Status.
+select maritalstatus, COUNT(*) AS total_count, SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count, (SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS attrition_rate
+from hr_employee
+group by maritalstatus;
+
+-- 14.	Find the Attrition Count & Percentage for Different Education Levels
+select education, COUNT(*) AS total_count, SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count, (SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS attrition_rate
+from hr_employee
+group by education;
+
+-- 15 Find the Attrition & Percentage Attrition for Business Travel.
+select businesstravel, COUNT(*) AS total_count, SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count, (SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS attrition_rate
+from hr_employee
+group by businesstravel;
+
+-- 16 Find the Attrition & Percentage Attrition for Various JobInvolvement
+select jobinvolvement, COUNT(*) AS total_count, SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count, (SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS attrition_rate
+from hr_employee
+group by  jobinvolvement;
+
+-- 17 Show Attrition Rate for Different JobSatisfaction
+select jobsatisfaction, COUNT(*) AS total_count, SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count
+from hr_employee
+group by  jobsatisfaction;
+-- 18 Find key reasons for attrititon in a company
+-- As income can be a reason for attrition lets make a column for income-group
+alter table hr_employee add column SPYTRENDS varchar(20);
+
+select jobinvolvement,jobsatisfaction, COUNT(*) AS total_count, (SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS attrition_rate
+from hr_employee
+group by jobinvolvement,jobsatisfaction
+order by attrition_rate;
+
+-- 19 Return all employee where WorkEx greater than 10, provided that they travel frequently, 
+-- WorkLifeBalance as Good and JobSatisfaction is Very High.
+select * from hr_employee 
+where workex>10 and businesstravel="Travel_Frequently" and worklifebalance="Good" and jobsatisfaction="Very High";
+
+-- 20 Write query to display who has better WorkLifeBalance , Married, Single or Divorced 
+-- provided that Performace_Rating is Outstanding. 
+select worklifebalance,maritalstatus,performance_rating,count(*) as res_dis
+from hr_employee 
+where performance_rating="Outstanding"
+group by worklifebalance, maritalstatus, performance_rating;
--- Assignments/mySQL/7th March Assignment SQL Day 4.sql
+++ Assignments/mySQL/7th March Assignment SQL Day 4.sql
@@ -4,39 +4,41 @@ use dharandb;
 drop table if exists employee;
 create table if not exists employee(emp_id varchar(10),emp_name varchar(30),salary double,dept_id varchar(10),
     manager_id varchar(10));
-insert into employee values("E1","Dharan",100000,"D1","M1");
-insert into employee values("E2","PK",200000,"D1","M1");
-insert into employee values("E3","Neel",300000,"D2","M2");
-insert into employee values("E4","Madhab",400000,"D2","M2");
-insert into employee values("E5","Puranjay",500000,"D3","M3");
-insert into employee values("E6","Kwatra",200000,"D3","M1");
-insert into employee values("E7","Tej",300000,"D1","M2");
-insert into employee values("E8","Pusthakala",400000,"D1","M1");
+INSERT INTO employee VALUES
+('E1','Divya Parasher',15000,'D1','M1'),
+('E2','Mukund',15000,'D1','M1'),
+('E3','Tiyanshi',55000,'D2','M2'),
+('E4','Pranshu',25000,'D2','M2'),
+('E5','Bhavi',20000,'D10','M3'),
+('E5','Tarandeep',35000,'D10','M3');
 
 -- 2. Department table
 drop table if exists department;
 create table if not exists department (dept_id varchar(20), dept_name varchar(20));
-insert into department value('D1','it support');
-insert into department value('D2','data insights');
-insert into department value('D3','data analysis');
-insert into department value('D4','salesforce');
+INSERT INTO department VALUES
+('D1','IT'),
+('D2','HR'),
+('D3','Finance'),
+('D4','Admin');
 
 -- 3. Manager table
 drop table if exists manager;
 create table if not exists manager (manager_id varchar(20), manager_name varchar(20), dept_id varchar(10));
-insert into manager values('M1','sukesh','D1');
-insert into manager values('M2','rahul','D2');
-insert into manager values('M3','naveen','D3');
-insert into manager values('M4','saurabh','D4');
-
+INSERT INTO manager VALUES
+('M1','Iron Man','D3'),
+('M2','Captain America','D4'),
+('M3','Thor','D1'),
+('M4','Natasha','D1');
 
 -- 4. Project table
 drop table if exists project;
 create table if not exists project(project_id varchar(20), project_name varchar(20), team_id varchar(20));
-insert into project values('P1','chatbot','T1');
-insert into project values('P2','ai','T2');
-insert into project values('P3','cloud','T3');
-insert into project values('P4','blockchain','T4');
+INSERT INTO project VALUES 
+('P1','Data Migration','E1'),
+('P1','Data Migration','E2'), 
+('P1','Data Migration','M3'),
+('P2','ETL Tool ','E1'), 
+('P2','ETL Tool','M4');
 
 -- 1. display all records from tables
 select * from department;
@@ -67,4 +69,54 @@ select emp.emp_name, dept.dept_name, m.manager_name, p.project_name
 from employee emp
 left outer join department dept on emp.dept_id=dept.dept_id 
 		inner join manager m on emp.manager_id=m.manager_id
-				left join project p on p.team_id=emp.emp_id;
+			   inner join project p on p.team_id=emp.emp_id;
+               
+-- 7. display all employees who have been assigned with projects having manager and department
+select emp.emp_name, dept.dept_name, m.manager_name, p.project_name
+from employee emp inner join department dept on emp.dept_id=dept.dept_id
+				  inner join manager m on emp.manager_id=m.manager_id
+				  inner join project p on p.team_id=emp.emp_id;
+                  
+-- 8. display all employees who have been assigned with projects and also 
+-- show all managers and all department.
+
+select emp.emp_name, dept.dept_name, m.manager_name, p.project_name
+from employee emp 
+inner join project p on p.team_id=emp.emp_id
+right join department dept on dept.dept_id=emp.dept_id
+right join manager m on emp.manager_id=m.manager_id
+union
+select emp.emp_name, dept.dept_name, m.manager_name, p.project_name
+from employee emp
+inner join project p on p.team_id=emp.emp_id
+right join department dept on dept.dept_id=emp.dept_id
+left join manager m on emp.manager_id=m.manager_id; 
+
+
+-- 9. SQL CROSS JOIN
+-- result set will include all rows from both tables, where each rows is the combination of the row in the first table
+-- with the row in the second table
+-- returns the cartesian product
+select * from employee;
+select * from department;
+
+select * -- emp.emp_name, dept.dept_name
+from employee emp cross join department dept;
+-- employee has 6 rows and 
+-- department has 4 rows 
+-- so cross join has 6*4=24 rows 
+
+-- Exercise: write a query to fetch all employee name and their corresponding dept_name
+-- also make sure to display the company name and company location corresponding to each employee. 
+
+drop table if exists company;
+create table if not exists company(	company_id varchar(20),    company_name varchar(50),    location varchar(50));
+insert into company values ("C0001","TEK Systems","Hyderabad");
+
+select * from company;
+select emp.emp_name, dept.dept_name, comp.company_name,comp.location from employee emp join department dept on emp.dept_id=dept.dept_id
+																					   cross join company comp;
+-- 10. NATURAL JOIN
+-- similar to inner join without condition (problem! - condition basis is unknown.)
+select * -- emp.emp_name, dept.dept_name 
+from employee emp natural join department dept;
