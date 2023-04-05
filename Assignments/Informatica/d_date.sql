34c2d76b09f60263e9e80004be5e488b76739a20
dharantej1 <pusthakala.dharan@gmail.com>
2023-04-05 20:52:54 +0530

    Informatica_and_Case_Study_Assignment

---
+++ Assignments/Informatica/d_date.sql
@@ -0,0 +1,14 @@
+CREATE TABLE d_date AS
+    SELECT
+       n AS Date_ID,
+       TO_DATE('31/12/2007','DD/MM/YYYY') + NUMTODSINTERVAL(n,'day') AS Full_Date,
+       TO_CHAR(TO_DATE('31/12/2007','DD/MM/YYYY') + NUMTODSINTERVAL(n,'day'),'DD') AS Days,
+       TO_CHAR(TO_DATE('31/12/2007','DD/MM/YYYY') + NUMTODSINTERVAL(n,'day'),'Mon') AS Month_Short,
+       TO_CHAR(TO_DATE('31/12/2007','DD/MM/YYYY') + NUMTODSINTERVAL(n,'day'),'MM') AS Month_Num,
+       TO_CHAR(TO_DATE('31/12/2007','DD/MM/YYYY') + NUMTODSINTERVAL(n,'day'),'Month') AS Month_Long,
+       TO_CHAR(TO_DATE('31/12/2007','DD/MM/YYYY') + NUMTODSINTERVAL(n,'day'),'YYYY') AS Year
+   FROM (
+   select level n
+   from dual
+   connect by level <= 2000
+   );
